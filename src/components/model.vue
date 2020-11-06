435db6e5d648e51a910f73e48c640c1f321a4d53
你的名字 <>
2020-11-06 09:58:07 +0800

    vue项目第一次提交的模板

---
+++ src/components/model.vue
@@ -0,0 +1,26 @@
+<template>
+  <teleport to="#modal">
+    <div id="center">
+      <h1>this is a modal</h1>
+    </div>
+  </teleport>
+</template>
+
+<script>
+export default {
+  name: 'model'
+}
+</script>
+
+<style scoped >
+#center {
+  width: 200px;
+  height: 200px;
+  background: red;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  margin-left: -100px;
+  margin-top: -100px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+<p>{{sum}}</p>
+  
+</template>
+
+<script>
+import { onMounted, reactive,toRefs } from 'vue'
+import BigNumber  from "bignumber.js";
+export default {
+    name:"Home",
+    setup(){
+        const data=reactive({
+            sum:null,
+        })
+        onMounted(()=>{
+            let x = new BigNumber(0.3333);
+            let sum=x.minus(0.2);
+            data.sum=sum.valueOf()*1;
+            console.log(typeof(data.sum),"sum")
+
+        })
+        return{
+            ...toRefs(data)
+
+        }
+
+    }
+
+}
+</script>
+
+<style>
+
+</style>
